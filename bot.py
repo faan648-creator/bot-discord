1436b30b8617dd64c799c3faf01105548b07e6f2
faan648-creator <faan648@gmail.com>
2026-09-03 15:00:50 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ intents.members = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 # ID Pesan list utama yang mau diedit otomatis (untuk command /done)
-LIST_MESSAGE_ID = 1542521523095740426  
+LIST_MESSAGE_ID = 1544980337036369951 
 
 @bot.event
 async def on_ready():

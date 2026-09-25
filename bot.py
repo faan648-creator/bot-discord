d66298a6c392106376f81355efb9f169ae286bda
faan648-creator <faan648@gmail.com>
2026-09-25 14:35:34 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -327,7 +327,7 @@ async def setuplist(interaction: discord.Interaction):
     format_list = (
         "LIST BOOST SERVER FISH IT X8 By <@617248535913693194> <@785872264100446210>\n\n"
         "14k/SLOT\n\n"
-        "KLOTER 25 ( 24 JAM )\n\n"
+        "KLOTER 26 ( 24 JAM )\n\n"
         "LIST MENGGUNAKAN Usn & Nick ROBLOX\n"
         "Contoh : zens1907\n\n"
         "1. -\n2. -\n3. -\n4. -\n5. -\n"
